57ac0d94871af0b8a6b052512c95c043a75d4929
jacovb <vbiljon@gmail.com>
2020-11-30 20:12:33 +0000

    upload image to note

--- src/aws-exports.js
+++ src/aws-exports.js
@@ -11,7 +11,9 @@ const awsmobile = {
     "aws_appsync_graphqlEndpoint": "https://te62vbjourhcfjnhoagyiot4xq.appsync-api.eu-west-2.amazonaws.com/graphql",
     "aws_appsync_region": "eu-west-2",
     "aws_appsync_authenticationType": "API_KEY",
-    "aws_appsync_apiKey": "da2-ngzd42c6zzaw5cirlpkwx66u6y"
+    "aws_appsync_apiKey": "da2-ngzd42c6zzaw5cirlpkwx66u6y",
+    "aws_user_files_s3_bucket": "note-image64403-dev",
+    "aws_user_files_s3_bucket_region": "eu-west-2"
 };
 
 
