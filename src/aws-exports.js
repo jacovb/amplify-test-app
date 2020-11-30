0a1026443a00ace38d9a1557257ab36025a6b46b
jacovb <vbiljon@gmail.com>
2020-11-30 06:50:56 +0000

    api added

--- src/aws-exports.js
+++ src/aws-exports.js
@@ -7,7 +7,11 @@ const awsmobile = {
     "aws_cognito_region": "eu-west-2",
     "aws_user_pools_id": "eu-west-2_PbB4Q7zD2",
     "aws_user_pools_web_client_id": "5on0a0c1hr7ugg0pscfsk6tj6g",
-    "oauth": {}
+    "oauth": {},
+    "aws_appsync_graphqlEndpoint": "https://te62vbjourhcfjnhoagyiot4xq.appsync-api.eu-west-2.amazonaws.com/graphql",
+    "aws_appsync_region": "eu-west-2",
+    "aws_appsync_authenticationType": "API_KEY",
+    "aws_appsync_apiKey": "da2-ngzd42c6zzaw5cirlpkwx66u6y"
 };
 
 
